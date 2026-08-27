3f0846944031ed665d9f2cfe43277ec7d40e2555
lifebook-beta <data.lifebook@gmail.com>
2026-08-27 15:08:46 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
-import 'app.dart';
+import 'app_demo.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const LifeBookApp());
+
+  runApp(const LifeBookDemoApp());
 }
